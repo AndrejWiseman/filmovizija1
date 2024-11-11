1664cf2b770193f996c611bb47c0b43096100401
Andrej <533737@gmail.com>
2024-11-11 12:20:22 +0100

    prvi

--- filmovizija1/urls.py
+++ filmovizija1/urls.py
@@ -6,5 +6,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('home.urls')),
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
